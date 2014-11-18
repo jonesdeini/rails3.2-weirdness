e9a4f1aa9b7fdf743feae525b7e144ac0089a397
jonesdeini <jonesdeini@gmail.com>
2014-11-18 01:23:18 -0500

    add a more realistic scope

--- app/models/bro.rb
+++ app/models/bro.rb
@@ -1,8 +1,8 @@
 class Bro < ActiveRecord::Base
   attr_accessible :cool_story
 
-  scope :hey_bro, -> do
-    where(nil)
-  end
+  scope :hey_bro, -> { where(nil) }
+
+  scope :with_cool_story, -> { where(cool_story: true) }
 
 end
